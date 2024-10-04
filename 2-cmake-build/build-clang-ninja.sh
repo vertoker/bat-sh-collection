adf68af1d0641e1ce86d2b852ad08f725c991613
vertoker <vertogpro@gmail.com>
2024-10-04 13:24:07 +0500

    fix 2-cmake

--- 2-cmake-build/build-clang-ninja.sh
+++ 2-cmake-build/build-clang-ninja.sh
@@ -2,8 +2,10 @@ echo build project using cmake
 echo using clang/clang++ compilers
 echo and ninja
 
-mkdir build/
-cd build/
+mkdir out/
+cd out/
 
 cmake .. -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -G "Ninja"
+ninja
+./executable
 
